6a808e151651bf2387253c20a67c2041a7ef089f
priyanshukumarjaipur-eng <priyanshukumarjaipur@gmail.com>
2026-06-30 17:56:22 +0530

    Time: 1 ms (98.54%) | Memory: 47.6 MB (61.93%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int r=matrix.length;
+        int c=matrix[0].length;
+        boolean row[]=new boolean[r];
+        boolean col[]=new boolean[c];
+        int x=0;
+        int y=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(matrix[i][j]==0){
+                    row[i]=true;
+                    col[j]=true;
+                }
+            }
+            
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+               if(row[i] || col[j]){
+                matrix[i][j]=0;
+               }
+            }
+            
+        }
+    }
+}
