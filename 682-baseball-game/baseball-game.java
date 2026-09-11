0d4e8320bed3deebecd3e7a7848627123c6478de
priyanshukumarjaipur-eng <priyanshukumarjaipur@gmail.com>
2026-09-11 17:58:37 +0530

    Time: 3 ms (79.57%) | Memory: 43.3 MB (91.62%) - LeetSync

---
+++ 682-baseball-game/baseball-game.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int calPoints(String[] op) {
+        Stack<Integer> st = new Stack<>();
+        for (String s : op) {
+            if (s.equals("C")) {
+                st.pop();
+            } else if (s.equals("D")) {
+                st.push(st.peek() * 2);
+            } else if (s.equals("+")) {
+                int a = st.pop();
+                int b = st.peek();
+                st.push(a);      
+                st.push(a + b);  
+            } else {
+                st.push(Integer.parseInt(s));
+            }
+        }
+        int sum = 0;
+        for (int num : st) {
+            sum += num;
+        }
+        return sum;
+    }
+}
